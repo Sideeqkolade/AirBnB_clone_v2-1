145ca307cf64e29224a7f95f41f2935c5afbb4d2
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-19 17:47:28 +0100

    Test

--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,14 @@
 import uuid
 from datetime import datetime
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+import models
 
-#Base = declarative_base()
+Base = declarative_base()
 class BaseModel:
-    #id = Column(String(60), nullable=False, primary_key=True)
-    #created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
-    #updated_at = Column((DATETIME, nullable=False, default=datetime.utcnow())
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
 
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
@@ -19,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            #storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -37,6 +38,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
