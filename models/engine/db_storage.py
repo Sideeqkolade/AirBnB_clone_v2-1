ebcfc79e8694a62acdd6eb4584dc8b122449492a
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-20 01:20:29 +0100

    Update models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,7 @@ Storage
 """
 from os import getenv
 from sqlalchemy import create_engine
-from sqlalchmemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from models.city import City
 from models.user import User
@@ -22,12 +22,12 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}:/3306/{}".format(
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
             getenv("HBNB_MYSQL_USER"), getenv("HBNB_MYSQL_PWD"),
             getenv("HBNB_MYSQL_HOST"), getenv("HBNB_MYSQL_DB"), pool_pre_ping=True))
 
         if getenv("HBNB_ENV") == 'test':
-             Base.metadata.dropall(self.__engine)
+             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """query on the current database session (self.__session)"""
@@ -45,4 +45,34 @@ class DBStorage:
                     key = obj.__class__.__name__ + '.' + obj.id
                     empty_dict[key] = obj
             return empty_dict
-        
+
+    def new(self, obj):
+        """
+         add the object to the current database session (self.__session)
+        """
+
+        self.__session.add(obj)
+
+    def save(self):
+        """
+         commit all changes of the current database session (self.__session)
+        """
+
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """
+        delete from the current database session obj if not None
+        """
+
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """
+        Reloads
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
--- models/state.py
+++ models/state.py
@@ -3,9 +3,21 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+from models.city import City
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class"""
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE")  == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all, delete')
+    elif getenv("HBNB_TYPE_STORAGE") == 'file':
+        name = ""
+        @property
+        def cities(self):
+            city_list = []
+            for key, value in models.storage.all().items():
+                if value.state_id == self.id:
+                    city_list.append(value)
+            return city_list
