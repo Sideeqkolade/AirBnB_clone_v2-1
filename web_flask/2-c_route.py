cdb79870a2c91372aa2b80fd183233edf5184f78
root <sideeqkolade@gmail.com>
2023-05-21 00:40:23 +0100

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,6 +4,8 @@ A script that starts the flask web application
 it must be listening on 0.0.0.0, port 5000
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text
+variable (replace underscore _ symbols with a space)
 You must use the option strict_slashes=False in your route definition
 """
 
@@ -13,12 +15,21 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """Displays 'Hello HBNB!'."""
     return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'."""
     return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return f"C {text}"
 
 
 if __name__ == "__main__":
